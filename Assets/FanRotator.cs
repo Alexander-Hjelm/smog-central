89049e8e4bae73164c1a0a1923fba1dc09814f38
marcus1337 <blurgas@hotmail.com>
2019-11-19 23:45:30 +0100

    legit fan

---
+++ Assets/FanRotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //float ang = 0;
+    float rotateSpeed = 40.0f;
+    int delayer = 0;
+
+    private void FixedUpdate()
+    {
+        Quaternion rot = transform.rotation;
+        rot *= Quaternion.Euler(Time.fixedDeltaTime* rotateSpeed, 0, 0);
+        transform.rotation = rot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
