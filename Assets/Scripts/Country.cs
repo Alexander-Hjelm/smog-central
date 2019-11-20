7121053b643f2958ad645003e33a75c2efa419b5
marcus1337 <blurgas@hotmail.com>
2019-11-20 18:52:42 +0100

    scripts

---
+++ Assets/Scripts/Country.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Country : MonoBehaviour //All countries have this script
+{
+    public int NumParticles { get; set; }
+    public float Area { get; set; }
+    private HashSet<GameObject> smogs;
+
+    public void AddSmog(GameObject smog) //If a particle raycasts into the country, call this foo
+    {
+        smogs.Add(smog);
+        NumParticles = smogs.Count;
+    }
+    public void RemoveSmog(GameObject smog)
+    {
+        smogs.Remove(smog);
+        NumParticles = smogs.Count;
+    }
+
+    public float GetCO2()
+    {
+        if(NumParticles == 0)
+        {
+            return 0;
+        }
+        return NumParticles/Area;
+    }
+
+    void Start()
+    {
+        smogs = new HashSet<GameObject>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+}
