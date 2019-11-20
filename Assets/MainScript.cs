412dc40148ebb1685fd87c6ecb5fa877ed9f0f2f
marcus1337 <blurgas@hotmail.com>
2019-11-20 14:15:33 +0100

    picutre reg

---
+++ Assets/MainScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+
+    //private List<GameObject fans;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+}
