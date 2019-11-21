a9820bc2357dbc5a4b76f2bc95005758921e6e77
marcus1337 <blurgas@hotmail.com>
2019-11-21 17:16:56 +0100

    vfx

--- Assets/ParticleScript.cs
+++ Assets/ParticleScript.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class ParticleScript : MonoBehaviour
 {
+    private ParticleSystem partSys;
+    private Country land;
 
     void Start()
     {
-        
+        partSys = GetComponent<ParticleSystem>();
+        land = GetComponent<Country>();
     }
 
     void Update()
     {
+        var emiss = partSys.emission;
+        var main = partSys.main;
+        main.maxParticles = 10;
+        emiss.rateOverTime = 2.0f;
+
+        if (land)
+        {
+            main.maxParticles = land.NumParticles;
+            if(land.NumParticles >= 2)
+            {
+                emiss.rateOverTime = land.NumParticles / 2+1;
+            }
+
+        }
 
     }
 }
