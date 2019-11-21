2dd94228e09a8cb16c2c7a6ea4619ed62a19b74a
marcus1337 <blurgas@hotmail.com>
2019-11-21 15:37:37 +0100

    fan aim

--- Assets/FanScript.cs
+++ Assets/FanScript.cs
@@ -6,48 +6,59 @@ public class FanScript : MonoBehaviour
 {
     public GameObject standsOnLand { get; set; }
     public GameObject aimsAtLand { get; set; }
-    
+
+    private RaycastHit hit;
+    private int layerMask;
+
 
     void checkLandUnder()
     {
-        int layerMask = LayerMask.GetMask("Country");
-        RaycastHit hit;
-
         if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, layerMask))
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            //Debug.DrawRay(transform.position, Vector3.down * hit.distance, Color.yellow);
             standsOnLand = hit.transform.gameObject;
         }
         else
         {
+            Debug.DrawRay(transform.position, Vector3.down * 10.0f, Color.red);
             standsOnLand = null;
         }
     }
 
     void checkAimingOnLand()
     {
-        int layerMask = LayerMask.GetMask("Country");
-        RaycastHit hit;
         aimsAtLand = null;
 
-        for(int i = 0; i < 80; i++)
+        Vector3 FromPos = transform.position;
+        FromPos.y += 1.0f;
+        Vector3 AimAt = transform.TransformDirection(Vector3.right);
+        AimAt.y = -4.0f;
+        AimAt.Normalize();
+
+        float yPiece = 1.0f / 80.0f;
+
+        for (int i = 0; i < 80; i++)
         {
-
+            AimAt.y += yPiece;
+            Debug.DrawRay(FromPos, AimAt * 5.0f, Color.magenta);
+            if (Physics.Raycast(FromPos, AimAt, out hit, Mathf.Infinity, layerMask))
+            {
+                aimsAtLand = hit.transform.gameObject;
+                return;
+            }
         }
-
-
     }
 
     void Start()
     {
-        
+        layerMask = LayerMask.GetMask("Country");
     }
 
 
     void Update()
     {
         checkLandUnder();
-        
+        checkAimingOnLand();
 
     }
 }
