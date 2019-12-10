6094b3755e1c4b2e9df711c1186792ea310541ae
marcus1337 <blurgas@hotmail.com>
2019-12-10 14:59:05 +0100

    unclear fix

--- Assets/Scripts/Country.cs
+++ Assets/Scripts/Country.cs
@@ -84,7 +84,8 @@ public class Country : MonoBehaviour //All countries have this script
             // Instantiate particle system
             particleObj = Instantiate(GetComponentInParent<ContinentScript>().particleObject,transform,true);
             particleObj.transform.localPosition = new Vector3(0, 0, 0);
-            particleObj.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            //particleObj.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            particleObj.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
             // Set stable levels
             if (carbonIntensity < 500f)
